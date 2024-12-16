012a815167832c90abad1092ee6ab17ef8469745
chandan0507 <chandanubale8@gmail>
2024-12-16 22:55:59 +0530

    included DOM dir which has dom basics

---
+++ DOM/index.js
@@ -0,0 +1,26 @@
+const selectHTwo = document.querySelector('h2')
+selectHTwo.setAttribute('class', 'newClass')
+
+
+selectListTwo = document.querySelectorAll('li')
+
+// Below is a function which loops through the selectListTwo querySelectorAll list and for each element it puts the newClass class
+
+// Node list has forEach method
+function changeTheList(sendTheElement) {
+    sendTheElement.forEach((element) => {
+        element.setAttribute('class', 'newClass')
+    });
+}
+
+changeTheList(selectListTwo)
+
+// Below is the best way to convert a nodeList into an array
+const newArry = Array.from(selectListTwo)
+newArry.filter( (chan) => {
+
+    // checking if attribute name is equal to bro, then only setting the class attribute as This-is-another-class
+    if (chan.getAttribute('name') === "bro"){
+        chan.setAttribute('class', 'This-is-another-class')
+    }
+})
